18402a3634ef6c1ac39344671eb1ba1b0e5c3dba
Mukesh Gupta <mukesh.gupta@agnicient.com>
2019-06-13 17:17:10 +0530

    Use of lightning-datatable in genericTable LWC.

--- LWC_Basic/force-app/main/default/lwc/genericTable/genericTable.js
+++ LWC_Basic/force-app/main/default/lwc/genericTable/genericTable.js
@@ -4,9 +4,19 @@ import getDatas from '@salesforce/apex/ContactController.getData'
 
 export default class GenericTable extends LightningElement {
     @track dataList;
+    @track tableLoadingState = true;
     @track error;
+    @track columns; 
     @api objectName;
     @api fields;
+
+    connectedCallback(){        
+        const col = [];
+        this.fields.split(',').forEach(item => {
+            col.push({label:item.replace('__c','').replace('_',' ').trim(),fieldName:item.trim()});
+        });
+        this.columns = col;
+    }
 
     @wire (getDatas,{fields:'$fields',objectName:'$objectName'})
     
@@ -21,5 +31,6 @@ export default class GenericTable extends LightningElement {
             // eslint-disable-next-line no-console
             console.log(error);
         }
+        this.tableLoadingState = false;
     }
 }
