e30c03e1d99336239b06ec22809991924714d3e5
Mukesh Gupta <mukesh.gupta@agnicient.com>
2019-06-28 20:02:56 +0530

    Create Generic Table that get object name and field from lightning app builder and draw table.

--- LWC_Basic/force-app/main/default/lwc/genericTable/genericTable.js
+++ LWC_Basic/force-app/main/default/lwc/genericTable/genericTable.js
@@ -1,35 +1,54 @@
+/* eslint-disable no-console */
 import { LightningElement,track,api,wire } from 'lwc';
 
-import getDatas from '@salesforce/apex/ContactController.getData'
+import getDatas from '@salesforce/apex/CommonDataTableController.getGenericObjectRecord'
 
 export default class GenericTable extends LightningElement {
-    @track dataList;
+    @track wrap = {LWCTabWrap:{tableColumn:{}}};
+    @track sortByName;
+    @track sortType='asc';    
     @track tableLoadingState = true;
+    @track iconName='utility:table';
     @track error;
-    @track columns; 
+    
     @api objectName;
     @api fields;
 
-    connectedCallback(){        
+    connectedCallback(){
+        if(this.objectName.indexOf('_')<0){
+            this.iconName = 'standard:'+this.objectName.toLowerCase();
+        }
+
         const col = [];
-        this.fields.split(',').forEach(item => {
+        this.fields.split(',').forEach((item,index) => {
+            if(index===0){
+                this.sortByName = item;
+            }
             col.push({label:item.replace('__c','').replace('_',' ').trim(),fieldName:item.trim()});
         });
-        this.columns = col;
+        this.wrap.LWCTabWrap.tableColumn = col;
     }
 
-    @wire (getDatas,{fields:'$fields',objectName:'$objectName'})
-    
-    wireData({error,data}){
+    @wire (getDatas,{
+        ObjectName:'$objectName',
+        fieldstoget:'$fields',
+        pagesize:10,
+        next:false,
+        prev:false,
+        off:0,
+        sortBy:'$sortByName',
+        sortType:'$sortType'
+    })    
+    wireData({data,error}){
         if(data){
-            this.dataList = data;
+            this.wrap = data;
+            console.log(this.wrap);
             this.error = undefined;
         }
         else if(error){
-            this.dataList=undefined;
+            this.wrap=undefined;
             this.error = error;
-            // eslint-disable-next-line no-console
-            console.log(error);
+            console.error(error);
         }
         this.tableLoadingState = false;
     }
